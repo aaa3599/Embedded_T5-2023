8a09bb655be9e87d50c9e232ce56aa02197944b6
aaa3599 <leducanh3599@gmail.com>
2023-07-09 07:04:10 +0700

    Vector

--- Day 11/Vector.cpp
+++ Day 11/Vector.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <map>
+#include <string>
 
 using namespace std;
 
+typedef struct node
+{
+    int value;
+    struct node *next;
+}node;
 
 
 int main(){
-    vector<int> array;
+    // vector<int> array;
+    list<int> array;
     // array.push_back(4);
     // array.push_back(8);
     // array.push_back(20);
@@ -15,17 +24,31 @@ int main(){
     // array.insert(array.begin()+2,77);
 
     // array.erase(array.begin()+2);
+    
     auto data = 10.8;
     array.assign(8,5);
 
-    int i;
-    for (i = 0; i < array.size(); i++)
+    
+    // for (int i = 0; i < array.size(); i++)
+    // {
+    //     printf("%d\n",array[i]);
+    // }
+    
+    
+    for( int item : array)
+        printf("%d  ",item);
+
+    map<string, int> SinhVien;
+
+    SinhVien["ID"]=101;
+    SinhVien["LOP"]= 7;
+    SinhVien["TUOI"]=12;
+
+    for (auto item:SinhVien)
     {
-        printf("%d\n",array[i]);
+        printf("\nKey: %s, value:%d, diachi:%p, size:%d",item.first.c_str(),item.second, &item, sizeof(item));
     }
     
-    // int item;
-    // for( int item : array)
-    //     printf("%d\n",item);
+
     return 0;
 }
